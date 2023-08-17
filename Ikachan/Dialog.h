1ecade61fc0f9f38af01fbe22b036950ea9f4b4d
Vladimir <vladimir.kosickij@gmail.com>
2023-08-17 18:12:18 +0300

    Version bump + gitignore fix

--- Ikachan/Dialog.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-//#include <windows.h>
-
-//INT_PTR CALLBACK DlgProc(HWND hWndDlg, UINT Msg, WPARAM wParam, LPARAM lParam);
--- Ikachan/Game.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//#include <windows.h>
-#include "WindowsWrapper.h"
-
-extern RECT grcLoading;
-
-BOOL Game(void);
